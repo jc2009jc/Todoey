079e4b59b55163286600ebae6d8e0e19e2272afa
Eva Cheng <geneeva@hotmail.com>
2018-07-03 17:24:38 +0800

    MVC version

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jimmy Cheng on 3/7/2018.
+//  Copyright © 2018 Jimmy Cheng. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}

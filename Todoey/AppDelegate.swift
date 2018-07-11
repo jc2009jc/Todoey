3869a4a7dccc06c666cdaaf233bad0a7ea957e45
Eva Cheng <geneeva@hotmail.com>
2018-07-11 18:50:32 +0800

    CoreData to remote

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        //print("didFinshLaunchingWithOptions")
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+//        let data = Data()
+//        data.name = "Angela"
+//        data.age = 12
+        
+        do {
+            let realm = try Realm()
+//            try realm.write {
+//                realm.add(data)
+//            }
+        } catch {
+            print ("Error initializing new realm, \(error)")
+        }
+        
+        
+        
         return true
     }
 
